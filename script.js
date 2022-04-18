42ae604d866a7a0777a2794cd63bafdf59c063d0
Vinny D <vdurham@prideseals.com>
2022-04-17 22:15:30 -0400

    added moment

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-var FamilyPicEl = document.querySelector("#family-picture")
-var ThanksEl = document.querySelector("#thank-you-text")
-var openingEl = document.querySelector(".opening")
-var resultsEl = document.querySelector(".results")
+var FamilyPicEl = document.querySelector("#family-picture");
+var ThanksEl = document.querySelector("#thank-you-text");
+var CurrentDay = document.querySelector("#current-day");
+var openingEl = document.querySelector(".opening");
+var resultsEl = document.querySelector(".results");
 var buttonEl = document.querySelector('#btn');
+
+var today = moment().format("dddd MMMM Do, YYYY");
+
+CurrentDay.append(today)
 
 
 var buildResults = function() {
-    openingEl.classList.add("none")
-    resultsEl.classList.remove("none")
-}
+    openingEl.classList.add("none");
+    resultsEl.classList.remove("none");
+};
 
 buttonEl.addEventListener('click', function () {
     buildResults();
