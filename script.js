08a5d69a3b647b25300b8a09194dd3e2264afda6
Vinny D <vdurham@prideseals.com>
2022-04-17 22:34:40 -0400

    fixed

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-var FamilyPicEl = document.querySelector("#family-picture");
-var ThanksEl = document.querySelector("#thank-you-text");
-var CurrentDay = document.querySelector("#current-day");
+var familyPicEl = document.querySelector("#family-picture");
+var thanksEl = document.querySelector("#thank-you-text");
+var currentDay = document.querySelector("#current-day");
 var openingEl = document.querySelector(".opening");
 var resultsEl = document.querySelector(".results");
 var buttonEl = document.querySelector('#btn');
 
 var today = moment().format("dddd MMMM Do, YYYY");
 
+var makeDay = function() {
+    currentDay.append(today);
+};
 
 var buildResults = function() {
     openingEl.classList.add("none");
     resultsEl.classList.remove("none");
 };
 
-CurrentDay.append(today)
-
 buttonEl.addEventListener('click', function () {
     buildResults();
 });
+
+makeDay();
